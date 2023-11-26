c2aa2b7189d071417bd212630280afff332b7117
Tien dat <tiendatdev12301@gmail.com>
2023-11-26 16:50:28 +0700

    fix bug exam set

--- src/main/java/com/app/lika/payload/request/CreateExamSetRequest.java
+++ src/main/java/com/app/lika/payload/request/CreateExamSetRequest.java
@@ -1,5 +1,6 @@
 package com.app.lika.payload.request;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
@@ -20,6 +21,7 @@ public class CreateExamSetRequest {
     @NotNull
     private Short quantityOfExam;
 
+    @Valid
     private List<CriteriaRequest> criteria;
 
     public List<CriteriaRequest> getCriteria() {
--- src/main/java/com/app/lika/payload/request/CriteriaRequest.java
+++ src/main/java/com/app/lika/payload/request/CriteriaRequest.java
@@ -1,6 +1,9 @@
 package com.app.lika.payload.request;
 
 import com.app.lika.model.question.Level;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
@@ -10,6 +13,8 @@ public class CriteriaRequest {
     private Long id;
 
     @NotNull
+    @Min(value = 1,message = "Criteria need quantity between 1 to 5")
+    @Max(value = 5,message = "Criteria need quantity between 1 to 5")
     private Short quantity;
 
     @NotNull
